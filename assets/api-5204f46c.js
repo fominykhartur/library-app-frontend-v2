fff0eeecd4ed614deced53229f38747f27b90fdb
fominykhartur <fominykhartur+GitHub@gmail.com>
2023-05-18 23:53:23 +0300

    deploy

---
+++ assets/api-5204f46c.js
@@ -0,0 +1,1 @@
+import{J as r}from"./index-f11659b4.js";async function s(t){return await r.get(t,{headers:{Authorization:`Bearer ${localStorage.getItem("jwt")}`}})}async function n(t,e){return await r.post(t,e,{headers:{Authorization:`Bearer ${localStorage.getItem("jwt")}`}})}export{s as g,n as p};
